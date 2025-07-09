05be61f0ec854a3576c6dfac757c78c1a0297d15
silas <silas@enzler.info>
2025-07-09 11:07:13 +0200

    UPDATE Request in RecipieService added

--- backend/src/main/java/ch/wiss/kochbuch/repository/RecipieIngredientRepository.java
+++ backend/src/main/java/ch/wiss/kochbuch/repository/RecipieIngredientRepository.java
@@ -1,10 +1,18 @@
 package ch.wiss.kochbuch.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ch.wiss.kochbuch.model.Ingredient;
 import ch.wiss.kochbuch.model.Recipie;
 import ch.wiss.kochbuch.model.RecipieIngredient;
+import jakarta.transaction.Transactional;
 
 public interface RecipieIngredientRepository extends JpaRepository<RecipieIngredient, Long> {
-void deleteByRecipie(Recipie recipie);
+    List<RecipieIngredient> findByRecipie(Recipie recipie);
+    RecipieIngredient findByRecipieAndIngredient(Recipie recipie, Ingredient ingredient);
+    
+    @Transactional
+    void deleteByRecipie(Recipie recipie);
 }
